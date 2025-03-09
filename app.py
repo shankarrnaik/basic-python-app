420208812bba60dcb6e4ef8d0bb80e04ccbee5a0
Shankar Naik <126056920+shankarrnaik@users.noreply.github.com>
2025-03-09 21:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ def greet(name):
 def main():
     # Run the greeting function
     print(greet("World"))
+    # Keep the script running
+    import time
+    while True:
+        time.sleep(20)
 
 if __name__ == '__main__':
     main()
